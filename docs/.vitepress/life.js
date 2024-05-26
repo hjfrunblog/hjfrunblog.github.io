a2c4387ac435db9883e71666277bb7d88328c516
hjfrun <hjfrun@gmail.com>
2024-05-26 17:20:21 +0800

    flight

--- docs/.vitepress/life.js
+++ docs/.vitepress/life.js
@@ -5,7 +5,8 @@ export function sidebarLife() {
       base: '/life/',
       items: [
         { text: '今天看了小米SU7', link: 'first-meet-su7' },
-        { text: '光伏阳光房开建啦', link: 'pv-sunroom' }
+        { text: '光伏阳光房开建啦', link: 'pv-sunroom' },
+        { text: '机票', link: 'flight' }
       ]
     }
   ]
