9cd2cf451c94d0b38ed55e469cc25115a01025da
hjfrun <hjfrun@gmail.com>
2024-06-10 23:24:58 +0800

    refracter

--- docs/.vitepress/life.js
+++ docs/.vitepress/life.js
@@ -6,8 +6,7 @@ export function sidebarLife() {
       items: [
         { text: '今天看了小米SU7', link: 'first-meet-su7' },
         { text: '光伏阳光房开建啦', link: 'pv-sunroom' },
-        { text: '英文标点符号', link: 'punctuation-mark' },
-        { text: '我的奶奶', link: 'grandma' }
+        { text: '英文标点符号', link: 'punctuation-mark' }
       ]
     }
   ]
