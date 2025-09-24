6d412e0f3c9b9103d36ce6004d2dfc0e00ce4020
hjfrun <hjfrun@gmail.com>
2025-09-24 18:57:38 +0800

    update fire

--- docs/.vitepress/fire.js
+++ docs/.vitepress/fire.js
@@ -1,6 +1,10 @@
 export function sidebarFire() {
   return [
     {
+      text: 'FIRE🔥',
+      link: '/fire/'
+    },
+    {
       text: '股票',
       base: '/fire/stock/',
       items: [{ text: '基础', link: 'basic' }]
