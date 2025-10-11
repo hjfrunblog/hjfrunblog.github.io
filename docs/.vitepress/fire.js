c8c74e293013527296a32e44c1a709666eecf70e
hjfrun <hjfrun@gmail.com>
2025-10-11 13:20:24 +0800

    enlighten

--- docs/.vitepress/fire.js
+++ docs/.vitepress/fire.js
@@ -9,7 +9,8 @@ export function sidebarFire() {
       base: '/fire/stock/',
       items: [
         { text: '2025复盘', link: 'review-2025' },
-        { text: '基础', link: 'basic' }
+        { text: '基础', link: 'basic' },
+        { text: '开窍', link: 'enlighten' }
       ]
     }
   ]
